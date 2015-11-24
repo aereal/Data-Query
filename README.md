fc24c35031984b2ec7cb7cbfacf0e3ac779be527
aereal <aereal@aereal.org>
2015-11-24 19:38:17 +0900

    minil new Data-Query

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Data::Query - It's new $module
+
+# SYNOPSIS
+
+    use Data::Query;
+
+# DESCRIPTION
+
+Data::Query is ...
+
+# LICENSE
+
+Copyright (C) aereal.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+aereal <aereal@aereal.org>
---
+++ lib/Data/Query.pm
@@ -0,0 +1,39 @@
+package Data::Query;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Data::Query - It's new $module
+
+=head1 SYNOPSIS
+
+    use Data::Query;
+
+=head1 DESCRIPTION
+
+Data::Query is ...
+
+=head1 LICENSE
+
+Copyright (C) aereal.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+aereal E<lt>aereal@aereal.orgE<gt>
+
+=cut
+
